738b0eb8ccc9b9d0cb27116b6c47efdb63f57b74
webwizardsusa <webwizardsusa@gmail.com>
2024-03-01 19:21:00 +0530

    register and login use jwt

--- models/User.js
+++ models/User.js
@@ -1,9 +1,44 @@
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
+
+
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+  };
+  
+  userSchema.methods.generateJWT = function() {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+  
+    return jwt.sign({
+      email: this.email,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, 'secret');
+  }
+
+  userSchema.methods.toAuthJSON = function() {
+    return {
+      _id: this._id,
+      email: this.email,
+      token: this.generateJWT(),
+    };
+  };
 
 module.exports = mongoose.model('User', userSchema);
